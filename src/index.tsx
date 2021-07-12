076676e7dd720a9455e8a1ebfdce8258d1f3caae
Àngel Delgado <adelgado@medysif.com>
2021-07-12 13:48:23 +0200

    pre eslint

--- src/index.tsx
+++ src/index.tsx
@@ -8,10 +8,10 @@ import registerServiceWorker from './registerServiceWorker';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>,
-    document.getElementById('root')
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
 );
 
 registerServiceWorker();
