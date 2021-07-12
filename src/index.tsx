b340b3767e96f334c468731279793e6077c6d5c3
Àngel Delgado <adelgado@medysif.com>
2021-07-12 12:59:30 +0200

    gitflow-feature-stash: add-addets

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,10 @@
+import './index.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+
+import App from './components/App';
+import registerServiceWorker from './registerServiceWorker';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
@@ -11,6 +14,7 @@ ReactDOM.render(
     document.getElementById('root')
 );
 
+registerServiceWorker();
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
