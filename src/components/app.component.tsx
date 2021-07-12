aa4972fb597dc0d969b1d8f816fd0cddf01ad0e0
Àngel Delgado <adelgado@medysif.com>
2021-07-12 13:04:41 +0200

    gitflow-feature-stash: renanimg

---
+++ src/components/app.component.tsx
@@ -0,0 +1,27 @@
+import './app.component.css';
+
+import React, { Component } from 'react';
+
+import logo from './../assets/images/logo.svg';
+
+class App extends Component {
+    public render() {
+        return (
+            <div className="app">
+                <header className="app-header">
+                    <img src={logo} className="app-logo" alt="logo" />
+                    <p>
+                        Edit <code>src/app.component.tsx</code> and save to reload.
+                    </p>
+                    <a className="app-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+                        Learn React
+                    </a>
+                    <p>Node Env: {process.env.NODE_ENV}</p>
+                    <p>Version: {process.env.REACT_APP_VERSION}</p>
+                </header>
+            </div>
+        );
+    }
+}
+
+export default App;
